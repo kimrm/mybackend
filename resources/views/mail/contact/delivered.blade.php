12a3f5fa1b3ae4a3ff57139a56e87dadfcefce74
Kim Rune Møller <kimrune@hotmail.no>
2025-01-06 12:45:07 +0100

    adds mail template for contact request delivered receipt

---
+++ resources/views/mail/contact/delivered.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+# Takk for din henvendelse, {{ $contactRequest->name }}!
+
+Din melding vil bli svart på så snart som mulig.
+
+Med vennlig hilsen<br>
+{{ config('app.name') }}
+</x-mail::message>
