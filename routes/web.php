b658d6863dfdc8525d1282c9468a93e81c59ee57
Kim Rune Møller <kimrune@hotmail.no>
2025-01-07 12:53:20 +0100

    adds API settings routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\APIController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/API', [APIController::class, 'edit'])->name('api.edit');
+    Route::post('/API', [APIController::class, 'store'])->name('api.store');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
