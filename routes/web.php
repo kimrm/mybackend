1ca28f63c5e57c090fc83a79e33da92c2607efbb
Kim Rune Møller <kimrune@hotmail.no>
2025-01-09 20:54:32 +0100

    add projects route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\APIController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,6 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
 
     Route::get('/API', [APIController::class, 'edit'])->name('api.edit');
     Route::post('/API', [APIController::class, 'store'])->name('api.store');
