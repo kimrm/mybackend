96ef33afc4df6f4546bb19515c1b83edb3a42b66
Kim Rune Møller <kimrune@hotmail.no>
2025-01-10 07:47:59 +0100

    small refactor, add projects resource route

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+
+    // Healthcheck route
     Route::get('/healthcheck', function () {
         return response()->json(['status' => 'ok']);
     });
-    Route::middleware('auth:sanctum')->post('/contact', 'App\Http\Controllers\Api\ContactRequestController@store');
+
+    // Authenticated routes group
+    Route::middleware('auth:sanctum')->group(function () {
+
+        // Contact request route
+        Route::post('/contact', 'App\Http\Controllers\Api\ContactRequestController@store');
+
+        // Projects routes
+        Route::resource('projects', 'App\Http\Controllers\Api\ProjectController')->except(['create', 'edit', 'destroy']);
+    });
 });
