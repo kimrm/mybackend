549719defa9d7767fcc45122f492e32972c2490c
Kim Rune Møller <kimrune@hotmail.no>
2025-01-14 13:07:53 +0100

    add chat and calendar routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ChatController;
+use App\Http\Controllers\Api\GoogleCalendarController;
 
 Route::prefix('v1')->group(function () {
 
@@ -15,7 +17,17 @@ Route::prefix('v1')->group(function () {
         // Contact request route
         Route::post('/contact', 'App\Http\Controllers\Api\ContactRequestController@store');
 
+        Route::resource('projects/featured', 'App\Http\Controllers\Api\FeaturedProjectController')
+            ->only(['index']);
+
         // Projects routes
-        Route::resource('projects', 'App\Http\Controllers\Api\ProjectController')->only(['index', 'show']);
+        Route::resource('projects', 'App\Http\Controllers\Api\ProjectController')
+            ->only(['index', 'show'])
+            ->parameters(['projects' => 'project:slug']);
+
+        Route::post('/chat', [ChatController::class, 'streamResponse']);
+
+        Route::get('/calendar/available-events', [GoogleCalendarController::class, 'listAvailableEvents']);
+        Route::get('/calendar/events', [GoogleCalendarController::class, 'listEvents']);
     });
 });
