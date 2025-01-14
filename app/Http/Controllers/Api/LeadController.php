17798ead60ff5132a0edbea3b7e5cd101629fbd2
Kim Rune Møller <kimrune@hotmail.no>
2025-01-14 13:04:06 +0100

    controller for receiving leads from website

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Lead;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        // name, email, phone, message, appointment, summary
+    }
+}
