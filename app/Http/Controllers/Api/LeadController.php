77a1a7363ca55d88fdf92cc5c4ed30fb507ee016
Kim Rune Møller <kimrune@hotmail.no>
2025-01-15 10:37:48 +0100

    dispatch CreateAppointment job

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -6,6 +6,7 @@ use App\Models\Lead;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\LeadCreatedResource;
+use App\Jobs\CreateAppointment;
 use App\Jobs\GenerateLeadSummary;
 
 class LeadController extends Controller
@@ -25,6 +26,7 @@ class LeadController extends Controller
 
         $lead = Lead::create($request->all());
 
+        dispatch(new CreateAppointment($lead));
         dispatch(new GenerateLeadSummary($lead));
 
         return new LeadCreatedResource($lead);
