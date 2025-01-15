c914034fa6a3ad8e1b22dbcc1625e945c2d198e8
Kim Rune Møller <kimrune@hotmail.no>
2025-01-15 09:18:29 +0100

    lead controller class

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Models\Lead;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\LeadCreatedResource;
+use App\Jobs\GenerateLeadSummary;
 
 class LeadController extends Controller
 {
+
     public function store(Request $request)
     {
-        // name, email, phone, message, appointment, summary
+        $request->validate([
+            'subject' => 'nullable|string',
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone' => 'nullable|string',
+            'questions' => 'nullable|string',
+            'message' => 'nullable|string',
+            'appointment' => 'nullable|date',
+        ]);
+
+        $lead = Lead::create($request->all());
+
+        dispatch(new GenerateLeadSummary($lead));
+
+        return new LeadCreatedResource($lead);
     }
 }
