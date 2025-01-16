63c5d04e2bd7ff4c9096a065d3cbebb6213dc6ae
Kim Rune Møller <kimrune@hotmail.no>
2025-01-16 14:51:53 +0100

    add Notifiable trait,

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Notifications\Notification;
+use Carbon\Carbon;
 
 class Lead extends Model
 {
+    use Notifiable;
+
     protected $fillable = [
         'subject',
         'name',
@@ -16,4 +21,15 @@ class Lead extends Model
         'appointment',
         'summary',
     ];
+
+    /**
+     * Route notifications for the mail channel.
+     *
+     * @return  array<string, string>|string
+     */
+    public function routeNotificationForMail(Notification $notification): array|string
+    {
+        // Return email address only...
+        return 'kim@kimrune.dev';
+    }
 }
