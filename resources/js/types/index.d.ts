57dfd283d15416c8025519b433e0e7c123155561
Kim Rune Møller <kimrune@hotmail.no>
2025-01-23 18:05:57 +0100

    add Project and Author interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,6 +5,22 @@ export interface User {
     email_verified_at?: string;
 }
 
+interface Project {
+    id: number;
+    title: string;
+    description: string;
+    created_at: date;
+    author: Author;
+    model: string;
+    model_id: number;
+    sort: number;
+}
+
+export interface Author {
+    id: number;
+    name: string;
+}
+
 export type PageProps<
     T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
